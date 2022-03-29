2ac502ce890ae01426a48a1943495839501d471d
Awakened-Redstone <awakenedredstone@gmail.com>
2022-03-29 16:30:23 -0300

    Fix environment annotation

--- src/main/java/com/awakenedredstone/subathon/server/SubathonServer.java
+++ src/main/java/com/awakenedredstone/subathon/server/SubathonServer.java
@@ -4,8 +4,9 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
+@Environment(EnvType.SERVER)
 public class SubathonServer implements DedicatedServerModInitializer {
+
     @Override
     public void onInitializeServer() {
     }
